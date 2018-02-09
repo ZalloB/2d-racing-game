136e3be0b6d2291f630e50216bfc107721d56605
beatriz zallo <beatriz.zallo@gmail.com>
2018-02-09 21:27:02 +0100

    First commit

---
+++ Assets/Game/Models/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle {
+
+	private float velocity;
+	private float control;
+	private float aceleration;
+	private float breakingReaction;
+
+	public float Velocity
+	{
+		get {return velocity; }
+		set {velocity = value; }
+	}
+
+	public float Control
+	{
+		get {return control; }
+		set {control = value; }
+	}
+
+	public float Aceleration
+	{
+		get {return aceleration; }
+		set {aceleration = value; }
+	}
+
+	public float BreakingReaction
+	{
+		get{return breakingReaction; }
+		set{breakingReaction = value; }
+	}
+}
