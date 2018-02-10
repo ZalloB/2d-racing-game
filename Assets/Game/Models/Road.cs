7023799a3637fe139bb8862f8c5de30cd4eec08d
Beatriz <beatriz.zallo@gmail.com>
2018-02-10 12:18:46 +0100

    first start scene and ui

---
+++ Assets/Game/Models/Road.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road {
+
+    private float velocityResistance;
+
+    public float VelocityResistance
+    {
+        get { return velocityResistance ; }
+        set { velocityResistance = value; }
+    }
+}
