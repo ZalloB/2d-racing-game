9960474b8faed8ac057b57d71a436e70b933d69f
Beatriz <beatriz.zallo@gmail.com>
2018-02-13 20:20:45 +0100

    level 0, music and roads prefabs

---
+++ Assets/Game/GameLevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLevelManager : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public float actualTime = 0;
+    public bool isStarted;
+    public Text textTime;
+
+    // Use this for initialization
+    void Start () {
+        isStarted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckPauseGame();
+        if(isStarted)
+        UpdateTimeUI();
+	}
+
+    void CheckPauseGame()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && !pauseMenu.activeSelf)
+        {
+            //SoundManager.instance.RandomizeSfx(pause);
+            //SoundManager.instance.musicSource.Pause();
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    void UpdateTimeUI()
+    {
+        actualTime += Time.deltaTime;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(actualTime);
+        if (timeSpan.Hours > 0)
+            textTime.text = string.Format("TIME: {0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+        else
+            textTime.text = string.Format("TIME: {1:D2}:{2:D2}", timeSpan.Minutes, timeSpan.Seconds);
+    }
+}
--- Assets/Game/VehicleController.cs
+++ Assets/Game/VehicleController.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class VehicleController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vehicle vehicle = new Vehicle();
+    public float power = 3;
+    public float maxspeed = 5;
+    public float turnpower = 2;
+    public float friction = 3;
+    public Vector2 curspeed;
+    Rigidbody2D actualRigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        vehicle.BreakingReaction = friction;
+        actualRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        curspeed = new Vector2(actualRigidbody2D.velocity.x, actualRigidbody2D.velocity.y);
+
+        if (curspeed.magnitude > maxspeed)
+        {
+            curspeed = curspeed.normalized;
+            curspeed *= maxspeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            actualRigidbody2D.AddForce(transform.up * power);
+            actualRigidbody2D.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            actualRigidbody2D.AddForce(-(transform.up) * (power / 2));
+            actualRigidbody2D.drag = friction;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * turnpower);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -turnpower);
+        }
+
+        Decelerate();
+
+    }
+
+    void Decelerate()
+    {
+        bool gas = false;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            gas = !gas;
+        
+
+        if (!gas)
+            actualRigidbody2D.drag = friction * 2;
+        
+    }
 }
